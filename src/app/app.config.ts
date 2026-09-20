bdd4f7ae73463903101c227be40b679164ed6a1e
PSPDesarrolloWeb <pspdesarrolloweb@gmail.com>
2026-09-20 00:08:37 -0500

    Conexión con Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,6 +12,13 @@ import { MessageService } from 'primeng/api';
 import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -32,6 +39,6 @@ export const appConfig: ApplicationConfig = {
         },
       },
     }),
-    MessageService,
+    MessageService, provideFirebaseApp(() => initializeApp({ projectId: "transvisionarioscloud01", appId: "1:649656379507:web:b45c7718cfbc2d90df2206", storageBucket: "transvisionarioscloud01.firebasestorage.app", apiKey: "AIzaSyAfWaKvEHjpcGPikFWeVxo1spaZht9v-vk", authDomain: "transvisionarioscloud01.firebaseapp.com", messagingSenderId: "649656379507", measurementId: "G-7DGTR03NH8", projectNumber: "649656379507", version: "2" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()),
   ],
 };
